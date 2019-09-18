399c993d7821ddc5c59349c9ce5e80c4c00e58c7
RAJEEV <rajeev-kalkunte.ranganathan@amadeus.com>
2019-09-18 21:48:13 +0530

    Flood fill implementation

--- CodinGame/src/com/cg/hardcoders/algos/FloodFill.java
+++ CodinGame/src/com/cg/hardcoders/algos/FloodFill.java
@@ -1,4 +1,43 @@
 package com.cg.hardcoders.algos;
 
 public class FloodFill {
+    private static final int row = 8;
+    private static final int column = 8;
+
+
+    private static void floodFillUtil(int[][] arr, int x, int y, int prevValue, int newValue) {
+        if (x < 0 || x >= row || y < 0 || y >= column) return;
+        if (arr[x][y] != prevValue) return;
+
+        arr[x][y] = newValue;
+        floodFillUtil(arr, x + 1, y, prevValue, newValue);
+        floodFillUtil(arr, x - 1, y, prevValue, newValue);
+        floodFillUtil(arr, x, y + 1, prevValue, newValue);
+        floodFillUtil(arr, x, y - 1, prevValue, newValue);
+    }
+
+    private static void floodFill(int[][] arr, int x, int y, int newValue) {
+        floodFillUtil(arr, x, y, arr[x][y], newValue);
+    }
+
+    public static void main(String[] args) {
+        int screen[][] = {{1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 0, 0},
+                {1, 0, 0, 1, 1, 0, 1, 1},
+                {1, 2, 2, 2, 2, 0, 1, 0},
+                {1, 1, 1, 2, 2, 0, 1, 0},
+                {1, 1, 1, 2, 2, 2, 2, 0},
+                {1, 1, 1, 1, 1, 2, 1, 1},
+                {1, 1, 1, 1, 1, 2, 2, 1},
+        };
+        int x = 4, y = 4, newC = 3;
+        floodFill(screen, x, y, newC);
+
+        System.out.println("Updated screen after call to floodFill: ");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++)
+                System.out.print(screen[i][j] + " ");
+            System.out.println();
+        }
+    }
 }
